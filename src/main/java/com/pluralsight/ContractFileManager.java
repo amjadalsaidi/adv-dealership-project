fc98485fb24bec5b41a915153c35e4f810fc0a1f
amjadalsaidi <146876370+amjadalsaidi@users.noreply.github.com>
2023-11-09 15:21:43 -0500

    add classes to my project

---
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class ContractFileManager {
+
+}
---
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+public class LeaseContract extends Contract {
+    private double value;
+    private double fee;
+
+    public LeaseContract(String date, String name, String email, Vehicle vehicle, double value, double fee) {
+        super(date, name, email, vehicle);
+        this.value = getVehicle().getPrice() *0.50;
+        this.fee = getVehicle().getPrice() *0.07;
+    }
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+
+
+    @Override
+    public double getTotalPrice() {
+        return (getVehicle().getPrice() - value) +fee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+    }
+
+}
